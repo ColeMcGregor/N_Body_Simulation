f269220e80d9fa8ac9d018056a461cefa9fb9167
ColeMcGregor <gc28mcgr@siena.edu>
2024-11-20 16:49:08 -0500

    made changes to body class, made a src folder, added skeletons for a few classes

---
+++ src/FileManager.cpp
@@ -0,0 +1,8 @@
+/**
+ * This file contains the implementation of the FileManager class, which is used to parse the input file for the creation of bodies in the simulation 
+ * 
+ * @requirements: input.txt file, must be in the same directory as the executable, and correctly formatted
+ * @output: output.txt file, will be in the same directory as the executable, used to simulate visualizations of the bodies in openGL
+ * 
+ * @author: Cole McGregor, Hawk Lindner, Brandon Trama
+ */
---
+++ src/Simulation.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Simulation.cpp
+ * @brief This file contains the implementation of the Simulation class, which is used to run the simulation
+ * 
+ * @algorithm: this simulation will take in an input file, parse the information, and create the bodies in the simulation
+ * it will then assign openMP threads to handle each body, and each thread will handle the calculations for that body
+ * each thread will also handle the output of that body to a file
+ * 
+ * @author: Cole McGregor, Hawk Lindner, Brandon Trama
+ * @requirements: FileManager class, which is used to parse the input file for the creation of bodies in the simulation, and the output of the bodies to a file
+ * @dependencies: body.cpp, filemanager.cpp
+ */
+
+
+
