ebb228c54d3f63a1baaa3748a41fa33276a166f6
HawkLindner <hm29lind@siena.edu>
2024-11-21 13:34:47 -0500

    fixed fileManager.h

--- src/FileManager.h
+++ src/FileManager.h
@@ -5,4 +5,5 @@
 class FileManager {
 public:
     void loadConfig(const string& filePath, vector<Body>& bodies, double& timestep, double& gravityMultiplier, int& iterations);
+    void FileManager::outputResults(const string& filePath);
 };
