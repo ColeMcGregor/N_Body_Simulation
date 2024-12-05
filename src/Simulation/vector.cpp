111de2ae2ed0b24572aa007e486b8804678f7d88
brandontrama <b18tram@siena.edu>
2024-12-04 21:55:35 -0500

    Corrected Vector class implementation in separate cpp file

--- src/Simulation/vector.cpp
+++ src/Simulation/vector.cpp
@@ -1,53 +1,46 @@
-#ifndef VECTOR_H
-#define VECTOR_H
-
 #include <cmath>
 #include <iostream>
-
-// Vector struct for holding and manipulating 3D coordinates
-struct Vector {
-    double x, y, z;
+#include "vector.h"
+#include "body.h"
 
     // Constructor: Initialize with (x, y, z), defaults to (0, 0, 0)
-    Vector(double x_ = 0.0, double y_ = 0.0, double z_ = 0.0)
+Vector::Vector(double x_, double y_, double z_)
         : x(x_), y(y_), z(z_) {}
 
     // Calculate magnitude (length of the vector)
-    double magnitude() const {
+double Vector::magnitude() const {
         return std::sqrt((x * x) + (y * y) + (z * z));
-    }
+}
+    
 
     // Vector addition
-    Vector operator+(const Vector& other) const {
-        return {x + other.x, y + other.y, z + other.z};
+Vector Vector::operator+(const Vector& other) const {
+  return Vector(x + other.x, y + other.y, z + other.z);
     }
 
     // Vector subtraction
-    Vector operator-(const Vector& other) const {
-        return {x - other.x, y - other.y, z - other.z};
+Vector Vector::operator-(const Vector& other) const {
+  return Vector(x - other.x, y - other.y, z - other.z);
     }
 
     // Scalar multiplication
-    Vector operator*(double scalar) const {
-        return {x * scalar, y * scalar, z * scalar};
-    }
+Vector Vector::operator*(double scalar) const {
+  return Vector(x * scalar, y * scalar, z * scalar);
+}
 
     // Scalar division
-    Vector operator/(double scalar) const {
-        return {x / scalar, y / scalar, z / scalar};
+Vector Vector::operator/(double scalar) const {
+  return Vector(x / scalar, y / scalar, z / scalar);
     }
 
     // Reset the vector to (0, 0, 0)
-    void reset() {
+void Vector::reset() {
         x = 0;
         y = 0;
         z = 0;
     }
 
     // Print the vector (x, y, z)
-    void print() const {
+void Vector::print() const {
         std::cout << x << "," << y << "," << z << std::endl;
     }
-};
-
-#endif // VECTOR_H
