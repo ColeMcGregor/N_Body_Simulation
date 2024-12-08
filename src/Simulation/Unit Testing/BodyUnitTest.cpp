26d4a3d69f4bc7671ddd8ec598c34d51189c4f8c
brandon.trama <b18tram@siena.edu>
2024-12-08 00:51:05 -0500

    Added trajectory declarations and removed test for resetForce

--- src/Simulation/Unit Testing/BodyUnitTest.cpp
+++ src/Simulation/Unit Testing/BodyUnitTest.cpp
@@ -45,14 +45,16 @@ void test_gravForce()
     Vector vel1(0, 0, 0);
     Vector accel1(0, 0, 0);
     Vector netForce1(0, 0, 0);
+    vector<Vector> trajectory1;
 
     Vector pos2(1, 0, 0);
     Vector vel2(0, 0, 0);
     Vector accel2(0, 0, 0);
     Vector netForce2(0, 0, 0);
+    vector<Vector> trajectory2;
 
-    Body body1(pos1, vel1, accel1, netForce1, 5.0, 1.0, 1.0, "planet", {}, {});
-    Body body2(pos2, vel2, accel2, netForce2, 10.0, 1.0, 1.0, "planet", {}, {});
+    Body body1(pos1, vel1, accel1, netForce1, 5.0, 1.0, 1.0, "planet", {}, trajectory1);
+    Body body2(pos2, vel2, accel2, netForce2, 10.0, 1.0, 1.0, "planet", {}, trajectory2);
 
     Vector expectedForce(6.6743e-10, 0, 0); // Use F = G * m1 * m2 / r^2 with G = 6.67430e-11, m1 = 5, m2 = 10, r = 1
     Vector actualForce = body1.gravForce(body2);
@@ -66,8 +68,9 @@ void test_applyForce()
     Vector vel(0, 0, 0);
     Vector accel(0, 0, 0);
     Vector netForce(0, 0, 0);
+    vector<Vector> trajectory;
 
-    Body body(pos, vel, accel, netForce, 10.0, 1.0, 1.0, "planet", {}, {});
+    Body body(pos, vel, accel, netForce, 10.0, 1.0, 1.0, "planet", {}, trajectory);
     Vector force(20.0, 0, 0);
     body.applyForce(force);
 
@@ -81,8 +84,9 @@ void test_update()
     Vector vel(0, 0, 0);
     Vector accel(1.0, 0, 0);
     Vector netForce(0, 0, 0);
+    vector<Vector> trajectory;
 
-    Body body(pos, vel, accel, netForce, 1.0, 1.0, 1.0, "planet", {}, {});
+    Body body(pos, vel, accel, netForce, 1.0, 1.0, 1.0, "planet", {}, trajectory);
     body.update(1.0);
 
     Vector expectedPosition(1.0, 0, 0); // position = 0 + velocity * 1 + 0.5 * acceleration * 1^2
@@ -98,14 +102,16 @@ void test_sumForces()
     Vector vel1(0, 0, 0);
     Vector accel1(0, 0, 0);
     Vector netForce1(0, 0, 0);
+    vector<Vector> trajectory1;
 
     Vector pos2(1, 0, 0);
     Vector vel2(0, 0, 0);
     Vector accel2(0, 0, 0);
     Vector netForce2(0, 0, 0);
+    vector<Vector> trajectory2;
 
-    Body body1(pos1, vel1, accel1, netForce1, 5.0, 1.0, 1.0, "planet", {}, {});
-    Body body2(pos2, vel2, accel2, netForce2, 10.0, 1.0, 1.0, "planet", {}, {});
+    Body body1(pos1, vel1, accel1, netForce1, 5.0, 1.0, 1.0, "planet", {}, trajectory1);
+    Body body2(pos2, vel2, accel2, netForce2, 10.0, 1.0, 1.0, "planet", {}, trajectory2);
 
     vector<Body> bodies = {body1, body2};
     Vector expectedForce(6.6743e-10, 0, 0); // Same as gravForce test
@@ -114,19 +120,6 @@ void test_sumForces()
     assert_vector_equal(expectedForce, actualForce, "Summing forces on body");
 }
 
-void test_resetForce()
-{
-    Vector pos(0, 0, 0);
-    Vector vel(0, 0, 0);
-    Vector accel(0, 0, 0);
-    Vector netForce(10.0, 0, 0);
-
-    Body body(pos, vel, accel, netForce, 1.0, 1.0, 1.0, "planet", {}, {});
-    body.resetForce();
-
-    Vector expectedForce(0, 0, 0);
-    assert_vector_equal(expectedForce, body.net_force, "Resetting net force");
-}
 
 int main()
 {
@@ -134,7 +127,6 @@ int main()
     test_applyForce();
     test_update();
     test_sumForces();
-    test_resetForce();
 
     std::cout << "\nSummary: " << passed_tests << "/" << total_tests << " tests passed.\n";
     return (total_tests == passed_tests) ? 0 : 1;
