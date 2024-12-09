35bbb8447d9fb0e81cf1e90a9c79dcef4b54cb50
brandon.trama <b18tram@siena.edu>
2024-12-08 23:12:59 -0500

    im an idiot

--- src/Simulation/body.h
+++ src/Simulation/body.h
@@ -10,10 +10,10 @@
 class Body
 {
 public:
-        const Vector position;     // where the body is
-        const Vector velocity;     // how fast it is moving in a given direction
-        const Vector acceleration; // how fast it is accelerating in a given direction
-        const Vector net_force;    // the net force acting on the body
+        Vector position;     // where the body is
+        Vector velocity;     // how fast it is moving in a given direction
+        Vector acceleration; // how fast it is accelerating in a given direction
+        Vector net_force;    // the net force acting on the body
         double mass;         // how much stuff it is made of
         double radius;       // how big it is from center to edge
         // special variables
@@ -22,10 +22,10 @@ public:
         std::vector<int> childrenIndices; // the indices of the bodies that are children of this body
         std::vector<Vector> trajectory;   // the trajectory of the body through time
 
-        Body(Vector &pos,
-             Vector &vel,
-             Vector &accel,
-             Vector &net_force,
+        Body(const Vector &pos,
+             const Vector &vel,
+             const Vector &accel,
+             const Vector &net_force,
              const double mass,
              const double radius,
              const double gravitationalMultiplier,
